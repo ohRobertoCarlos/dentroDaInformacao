1a1210446c2808937465e42836bdc0a5313aa86e
Roberto Carlos <robertocarlosdeveloper@gmail.com>
2021-05-24 22:09:25 -0300

    construindo layout sobre

--- dentro_da_informacao.sql
+++ dentro_da_informacao.sql
@@ -7,13 +7,8 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
-
-CREATE TABLE `contato` (
-  `id` int(11) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `telefone` varchar(15) NOT NULL,
-  `id_usuario` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE DATABASE IF NOT EXISTS `dentro_da_informacao` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `dentro_da_informacao`;
 
 CREATE TABLE `noticia` (
   `id` int(11) NOT NULL,
@@ -27,9 +22,6 @@ CREATE TABLE `noticia` (
   `slug` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO `noticia` (`id`, `titulo`, `subtitulo`, `texto_conteudo`, `id_autor`, `thumbnail`, `data_publicacao`, `descricao`, `slug`) VALUES
-(1, 'River-Plate improvisa jogador de linha no gol', 'Lorem ipsum dolor sit amet, consectetur adiLorem ipsu', 'Lorem ipsum dolor sit amet, consectetur adiLorem ipsum dolor sit amet, consectetur adiLorem ipsum dolor sit amet, consectetur adiLorem ipsum dolor sit amet, consectetur adi', 1, 'resources/images/futebol.jpg', '2021-05-19', 'river...', 'river-improvisa'),
-(2, 'Mc morre por fato curioso', 'r adiLorem ipsum dolor sit amet, consectet', 'r adiLorem ipsum dolor sit amet, consectetur adiLorem ipsum dolor sit amet, consectetur adiLorem ipsum dolor sit amet, consectetur adiLorem ipsum dolor sit amet, consectetur adiLorem ipsum dolor sit amet, consectetur adiLorem ipsum dolor sit amet, consectetu', 1, 'resources/images/futebol.jpg', '2021-05-19', 'A polícia investiga, mas causa da morte pode ter sido aventura sexual', 'mc-morre-fato-curioso');
 
 CREATE TABLE `usuario` (
   `id` int(11) NOT NULL,
@@ -39,9 +31,6 @@ CREATE TABLE `usuario` (
   `telefone` varchar(60) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO `usuario` (`id`, `nome`, `is_admin`, `email`, `telefone`) VALUES
-(1, 'Roberto', 1, '', ''),
-(3, 'jose', 0, 'jose@gmail.com', '62999999999');
 
 CREATE TABLE `usuario_admin` (
   `id` int(11) NOT NULL,
@@ -50,13 +39,6 @@ CREATE TABLE `usuario_admin` (
   `id_usuario` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO `usuario_admin` (`id`, `email_login`, `senha`, `id_usuario`) VALUES
-(1, 'roberto@email.com', '1234', 1);
-
-
-ALTER TABLE `contato`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_usuario` (`id_usuario`);
 
 ALTER TABLE `noticia`
   ADD PRIMARY KEY (`id`),
@@ -69,22 +51,6 @@ ALTER TABLE `usuario_admin`
   ADD PRIMARY KEY (`id`),
   ADD KEY `id_usuario` (`id_usuario`);
 
-
-ALTER TABLE `contato`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
-ALTER TABLE `noticia`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
-ALTER TABLE `usuario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
-ALTER TABLE `usuario_admin`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
-
-ALTER TABLE `contato`
-  ADD CONSTRAINT `contato_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`);
 
 ALTER TABLE `noticia`
   ADD CONSTRAINT `noticia_ibfk_1` FOREIGN KEY (`id_autor`) REFERENCES `usuario_admin` (`id`);
---
+++ src/js/scripts.js
@@ -0,0 +1,1 @@
+let todasNoticias = document.getElementsByClassName('todas-noticias');
